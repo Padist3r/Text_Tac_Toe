35d82156d129596d953b1b11c9312229456be892
Patrick Brennan <padist3r@gmail.com>
2020-11-12 11:19:35 +0000

    game finished

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import random
 
 
 def bot(string):
+    """Randomly selects a place for the computers turn."""
     if string == "":
         return None
     else:
@@ -9,8 +10,40 @@ def bot(string):
         return string[r]
 
 
-def check_win(string):
-    pass
+def check_win(string: str) -> str:
+    """Takes the board string and converts it into lists that can be checked
+    to determine a win, loose or draw"""
+    game_state = [[], [], []]
+    for j in range(0, len(string) - 1):
+        if string[j] in "123456789XO-":
+            if j < 11:
+                game_state[0].append(string[j])
+            elif 11 < j < 24:
+                game_state[1].append(string[j])
+            else:
+                game_state[2].append(string[j])
+
+    for line in game_state:
+        if "X" not in line and "-" not in line:
+            return "O"
+        elif "O" not in line and "-" not in line:
+            return "X"
+    if game_state[0][0] == game_state[1][1] == game_state[2][2] == "X" or \
+            game_state[0][2] == game_state[1][1] == game_state[2][0] == "X":
+        return "X"
+    elif game_state[0][0] == game_state[1][1] == game_state[2][2] == "O" or \
+            game_state[0][2] == game_state[1][1] == game_state[2][0] == "O":
+        return "O"
+    elif game_state[0][0] == game_state[1][0] == game_state[2][0] == "X" or \
+            game_state[0][1] == game_state[1][1] == game_state[2][1] == "X" or \
+            game_state[0][2] == game_state[1][2] == game_state[2][2] == "X":
+        return "X"
+    elif game_state[0][0] == game_state[1][0] == game_state[2][0] == "O" or \
+            game_state[0][1] == game_state[1][1] == game_state[2][1] == "O" or \
+            game_state[0][2] == game_state[1][2] == game_state[2][2] == "O":
+        return "O"
+    else:
+        return "Draw"
 
 
 game_on = True
@@ -25,20 +58,40 @@ while game_on:
         board = board.replace(user, "X", 1)
         bot_board = bot_board.replace(user, "", 1)
 
-        bot1 = bot(bot_board)
-        print("Computer chooses...")
-        board = board.replace(bot1, "O", 1)
-        bot_board = bot_board.replace(bot1, "", 1)
+        # checks to see if the last game space has been taken
+        if bot_board == "":
+            pass
+        else:
+            bot1 = bot(bot_board)
+            board = board.replace(bot1, "O", 1)
+            bot_board = bot_board.replace(bot1, "", 1)
 
-        # print(board)
+        # changes the board so it looks a bit nicer
         show_board = board
         for i in board:
             if i in "123456789":
-                show_board = show_board.replace(i, " ", 1)
+                show_board = show_board.replace(i, "-", 1)
         print(show_board)
-        # resets bot1 for the next turn
-        del bot1
+        # Checks to see if there is a winner
+        check = check_win(show_board)
+        if check == "X":
+            print("X WINS!!")
+            game_on = False
+        elif check == "O":
+            print("O Wins!!")
+            game_on = False
+        elif check == "DRAW":
+            print("DRAW GAME!!")
+            game_on = False
+        # resets variables for the next turn
+        try:
+            del bot1
+            del check
+        except NameError:
+            pass
 
     else:
+        # If there is an incorrect input the board will be displayed and the
+        # user asked for an input
         print(board)
         print("Please place an X in an appropriate place")
